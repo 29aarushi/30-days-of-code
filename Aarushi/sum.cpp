835d274fe5c001ad6f4e07b697491fc6dd578bbf
29aarushi <103803541+29aarushi@users.noreply.github.com>
2022-05-25 21:48:26 +0530

    Create sum.cpp

---
+++ Aarushi/sum.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+#define ll long long
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    ll n;
+	    cin>>n;
+	    ll nn= n;
+	    
+	    int sum= 0;
+	    while(nn>0){
+	        sum+= nn%10;
+	        nn/=10;
+	    }
+	    
+	    if(n%sum) cout<<"No\n";
+	    else cout<<"Yes\n";
+	    
+	}
+	return 0;
+}
